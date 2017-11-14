33bdf5071bdcc00af6c6459d7bfbdca1b1822125
mabidar <caza.ahmed@gmail.com>
2017-11-14 22:10:43 +0000

    init load

---
+++ backend/src/main/java/com/mabidar/pmb/PmbApplication.java
@@ -0,0 +1,12 @@
+package com.mabidar.pmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmbApplication.class, args);
+	}
+}
