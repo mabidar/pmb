48378e575b82cb2b6f0d605e94b9491938adbfc1
mabidar <caza.ahmed@gmail.com>
2017-11-14 22:50:01 +0000

    Spring Boot generate WAR file in order  to Deploy it in Tomcat

--- backend/src/main/java/com/mabidar/pmb/PmbApplication.java
+++ backend/src/main/java/com/mabidar/pmb/PmbApplication.java
@@ -2,10 +2,17 @@ package com.mabidar.pmb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PmbApplication {
+public class PmbApplication extends SpringBootServletInitializer  {
 
+	 @Override
+	    protected SpringApplicationBuilder  configure(SpringApplicationBuilder application) {
+	        return application.sources(PmbApplication.class);
+	    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PmbApplication.class, args);
 	}
